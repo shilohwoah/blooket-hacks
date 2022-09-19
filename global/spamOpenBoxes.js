435d0a0886f3cfecb4aeb7180891568de18449f2
shilohwoah <112996691+shilohwoah@users.noreply.github.com>
2022-09-20 10:59:32 +1200

    Update spamOpenBoxes.js
    i want to have 500 tokens each time i ckick on it it resets

--- global/spamOpenBoxes.js
+++ global/spamOpenBoxes.js
@@ -10,7 +10,8 @@
         try {
             let n = window.webpackJsonp.map(e => Object.keys(e[1]).map(t => e[1][t])).reduce((e, t) => [...e, ...t], []).find(e => /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/.test(e.toString()) && /\(new TextEncoder\)\.encode\(\"(.+?)\"\)/.test(e.toString())).toString();
             e({
-                blooketBuild: n.match(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/)[0],
+                blooketBuild: n.match(/\w{8}-\w{4}-\w{4
+                                      2}/)[0],
                 secret: n.match(/\(new TextEncoder\)\.encode\(\"(.+?)\"\)/)[1]
             })
         } catch {
